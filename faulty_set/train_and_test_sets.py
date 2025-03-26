1f47a2100ccf168a2352426444d5d0c3d7a7307d
Andrew Krapivin <andrewakrapivin@gmail.com>
2025-03-26 15:11:45 +0000

    Big transformer data generation settings

--- faulty_set/train_and_test_sets.py
+++ faulty_set/train_and_test_sets.py
@@ -5,9 +5,9 @@ import sys
 num_properties = 4
 num_values = 3
 # This is the number of potential sets evaluated by an agent in a sequence
-length_sequence = 30
-num_sequences_train = 100
-num_sequences_test = 500
+length_sequence = 50
+num_sequences_train = 60000
+num_sequences_test = 1000
 
 # Experiment 1 is making the training data have faulty agents
 # Then test using perfect agent
@@ -23,16 +23,31 @@ for i in range(num_properties):
 # 1. Have the card distribution for train set be the same as test set.
 # That is, we essentially pick a random faulty agent to generate set/no_set for each sequence,
 # But then each sequence is evaluated by a consistent faulty agent
-train_sequences, val_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_train, length_sequence, random_single_fault_masks, random_single_fault_masks_with_perfect, val=True)
-test_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_test, length_sequence, perfect_mask, random_single_fault_masks_with_perfect)
+# train_sequences, val_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_train, length_sequence, random_single_fault_masks, random_single_fault_masks_with_perfect, val=True)
+# test_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_test, length_sequence, perfect_mask, random_single_fault_masks_with_perfect)
 
-# print(train_sequences.shape)
-# sys.exit(1)
-torch.save(train_sequences, "./transcendenceGPT/data/card_set/train.pt")
-torch.save(val_sequences, "./transcendenceGPT/data/card_set/val.pt")
-torch.save(test_sequences, "./transcendenceGPT/data/card_set/test.pt")
+# # print(train_sequences.shape)
+# # sys.exit(1)
+# torch.save(train_sequences, "./transcendenceGPT/data/card_set/train.pt")
+# torch.save(val_sequences, "./transcendenceGPT/data/card_set/val.pt")
+# torch.save(test_sequences, "./transcendenceGPT/data/card_set/test.pt")
 
-# 2. Have the card distributions be different, where its picked to be roughly 50% correct answers for the faulty agent. This is probably even more challenging for the AI
+# # 2. Have the card distributions be different, where its picked to be roughly 50% correct answers for the faulty agent. This is probably even more challenging for the AI
 # train_sequences = game.generate_faulty_check_sequences(num_sequences_train, length_sequence, random_single_fault_masks)
 # test_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_test, length_sequence, perfect_mask, random_single_fault_masks_with_perfect)
 
+# torch.save(train_sequences, "./transcendenceGPT/data/card_set2/train.pt")
+# torch.save(val_sequences, "./transcendenceGPT/data/card_set2/val.pt")
+# torch.save(test_sequences, "./transcendenceGPT/data/card_set2/test.pt")
+
+train_sequences, val_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_train, length_sequence, random_single_fault_masks, random_single_fault_masks_with_perfect, val=True, set_probability = 0.8)
+test_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_test, length_sequence, perfect_mask, random_single_fault_masks_with_perfect, set_probability = 0.8)
+test_in_dist_sequences = game.generate_faulty_one_way_check_faulty_another(num_sequences_test, length_sequence, random_single_fault_masks, random_single_fault_masks_with_perfect, set_probability = 0.8)
+
+# print("train", train_sequences.shape)
+# print("val", val_sequences.shape)
+# print("test", test_sequences.shape)
+torch.save(train_sequences, "./transcendenceGPT/data/card_set_50x50_big/train.pt")
+torch.save(val_sequences, "./transcendenceGPT/data/card_set_50x50_big/val.pt")
+torch.save(test_sequences, "./transcendenceGPT/data/card_set_50x50_big/test.pt")
+torch.save(test_in_dist_sequences, "./transcendenceGPT/data/card_set_50x50_big/test_in_dist.pt")
